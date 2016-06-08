1261f0a4366f3e7777c955ce002403ea111bf41b
aec4444 <aec4444@gmail.com>
2016-06-08 16:20:20 -0400

    add more teams to quick link

--- Scripts/app/app.factories.js
+++ Scripts/app/app.factories.js
@@ -61,8 +61,8 @@
   return {
     defaultTeam: "10",
     teamKeys: teamKeys,
-    //statsUrl: "http://gamechanger-aec4444.rhcloud.com",
-    statsUrl: "http://localhost:8081",
+    statsUrl: "http://gamechanger-aec4444.rhcloud.com",
+    //statsUrl: "http://localhost:8081",
     getKeys: function(team) {
       var keys = teamKeys[team];
       return keys || defaultKeys;
